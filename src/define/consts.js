4e7fc0e62d5b0289eed982eacad6e3f7796fa72f
EHEH <81672416+eunhoya@users.noreply.github.com>
2022-06-22 14:20:19 +0900

    .

--- src/define/consts.js
+++ src/define/consts.js
@@ -4,12 +4,12 @@ export default {
     PAGE_SIZE: 10,
     PAGE_LIST_SIZE: 10,
   },
-  //   urls: {
-  //     ADMIN_API_SERVER: 'http://192.168.1.20:3013/api',
-  //     IMAGE_SERVER: 'http://192.168.1.20:81',
-  //   },
   urls: {
-    ADMIN_API_SERVER: 'http://192.168.1.20:3023/api',
+    ADMIN_API_SERVER: 'http://192.168.1.20:3013/api',
     IMAGE_SERVER: 'http://192.168.1.20:81',
   },
+  // urls: {
+  //   ADMIN_API_SERVER: 'http://192.168.1.20:3023/api',
+  //   IMAGE_SERVER: 'http://192.168.1.20:81',
+  // },
 };
