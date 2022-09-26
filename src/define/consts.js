03e115ebbb749cb122dd48446ff3504ef95d39cc
espeniel76 <espeniel69@gmail.com>
2022-09-26 12:05:13 +0900

    테스트 배포

--- src/define/consts.js
+++ src/define/consts.js
@@ -4,16 +4,16 @@ export default {
 		PAGE_SIZE: 10,
 		PAGE_LIST_SIZE: 10,
 	},
-	urls: {
-		ADMIN_API_SERVER: "http://192.168.1.20:3013/api",
-		IMAGE_SERVER: "http://192.168.1.20:81",
-	},
+	// urls: {
+	// 	ADMIN_API_SERVER: "http://192.168.1.20:3013/api",
+	// 	IMAGE_SERVER: "http://192.168.1.20:81",
+	// },
 	// urls: {
 	// 	ADMIN_API_SERVER: "http://192.168.1.20:3023/api",
 	// 	IMAGE_SERVER: "http://192.168.1.20:81",
 	// },
-	// urls: {
-	// 	ADMIN_API_SERVER: "https://adminapi.ttaom.com/api",
-	// 	IMAGE_SERVER: "https://s3.ap-northeast-2.amazonaws.com/image.ttaom.com",
-	// },
+	urls: {
+		ADMIN_API_SERVER: "https://adminapi.ttaom.com/api",
+		IMAGE_SERVER: "https://s3.ap-northeast-2.amazonaws.com/image.ttaom.com",
+	},
 };
